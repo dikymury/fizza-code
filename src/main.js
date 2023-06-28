2b7791c43d1fa63f615981b3c767996521e3bcb8
dikim <diki.liki@mologiz.com>
2023-06-28 17:09:50 +0700

    update setting 3

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from "vue";
 import "./assets/styles/app.css";
 import "./assets/styles/css/main.css";
 import App from "./App.vue";
+import SwitchViewApp from "./install";
+App.use(SwitchViewApp);
 createApp(App).mount("#app");
