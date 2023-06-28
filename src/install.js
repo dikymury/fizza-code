1ebc60f84815b264d66ae25fbc5dc73aa67fe142
dikim <diki.liki@mologiz.com>
2023-06-28 17:02:07 +0700

    update setting 2

--- src/install.js
+++ src/install.js
@@ -1,16 +1,14 @@
 import SwitchView from "./components/SwitchView.vue";
-
 const SwitchViewApp = {
   install(Vue) {
-    // Let's register our component globally
-    // https://vuejs.org/v2/guide/components-registration.html
+    // Vue is a Vue Constructor, not an Vue instance. This plugin requires passing the main app Vue instance as options parameter.
+    // Vue.component need to be called BEFORE new Vue() in main.js...
     Vue.component("switch-view", SwitchView);
   },
 };
-
+export default SwitchViewApp;
+export { SwitchViewApp };
 // Automatic installation if Vue has been added to the global scope.
 if (typeof window !== "undefined" && window.Vue) {
   window.Vue.use(SwitchViewApp);
 }
-
-export default SwitchViewApp;
--- src/main.js
+++ src/main.js
@@ -2,6 +2,4 @@ import { createApp } from "vue";
 import "./assets/styles/app.css";
 import "./assets/styles/css/main.css";
 import App from "./App.vue";
-import SwitchViewApp from "./install";
-App.use(SwitchViewApp);
 createApp(App).mount("#app");
